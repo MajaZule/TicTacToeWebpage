beec6bfaf6b492049d1d4335db631a2fe30c2c71
MajaZule <44669001+MajaZule@users.noreply.github.com>
2019-08-07 10:06:33 +0200

    Added components and bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { GridComponent } from './grid/grid.component';
+import { EndgameComponent } from './endgame/endgame.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    GridComponent,
+    EndgameComponent
   ],
   imports: [
     BrowserModule
