2e0ea1da937a4e55a99847ea7fa7f44d3c7167cb
Maja Zule <maja.zule@realcore.si>
2019-08-21 15:09:13 +0200

    did sth that does not work

--- src/app/grid/grid.component.ts
+++ src/app/grid/grid.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { PlayerDataService, StateType } from '../player-data.service';
-import { Action } from 'rxjs/internal/scheduler/Action';
 @Component({
   selector: 'app-grid',
   templateUrl: './grid.component.html',
@@ -14,12 +13,21 @@ export class GridComponent implements OnInit {
   yourTurn : boolean;
   opponentsTurn : boolean;
   playerName: string;
+  player1;
+  player2;
+  value: string;
+  coorX: number;
+  coorY: number;
+  x: number;
+  y: number;
 
   constructor(private playerService: PlayerDataService) { }
 
   ngOnInit() {
     this.playerNumber = this.playerService.responseData.Players.length;
     this.playerName = this.playerService.playerName;
+    this.player1 = {name: this.playerName[0], symbol: 'X'};
+    this.player2 = {name: this.playerName[1], symbol: 'O'};
     
     this.interval = setInterval(() => {
       this.playerService.state();
@@ -27,9 +35,12 @@ export class GridComponent implements OnInit {
     }, 500);
   }
 
-  onGridClick(x : number, y : number)  {
+  onGridClick(x, y) {
     console.log("Clicked on x:" + x + ",y:" + y);
     this.playerService.action(x, y);
+    this.coorX = x;
+    this.coorY = y;
+    this.updateBoard(event);
   }
 
   onStateChanged() {
@@ -37,6 +48,16 @@ export class GridComponent implements OnInit {
     this.isFirstPlayer = this.playerService.playerName === this.playerService.responseData.Players[0];
     this.yourTurn = (this.state === StateType.Player1Turn && this.isFirstPlayer) || (this.state == StateType.Player2Turn && !this.isFirstPlayer);
     this.opponentsTurn = (this.state === StateType.Player2Turn && this.isFirstPlayer) || (this.state == StateType.Player1Turn && !this.isFirstPlayer);
+  }
+
+  updateBoard(event: any) {
+    this.value = event.target.value = '';
+
+    if (this.isFirstPlayer && StateType.Player1Turn) {
+      this.value = this.player1.symbol;
+    } else if (!this.isFirstPlayer && StateType.Player2Turn) {
+      this.value = this.player2.symbol;
+    }
   }
 
 }
