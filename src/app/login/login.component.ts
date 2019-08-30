25a185993a6e446521a9cf6f6c9ce26ba756c037
Maja Zule <maja.zule@realcore.si>
2019-08-30 16:09:56 +0200

    TicTacToe animation (part)

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -18,6 +18,15 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
       transition('void => visible', [
         animate('2s ease-in')
       ])
+    ]),
+    trigger('flyIn', [
+      state('in', style({
+        transform: 'translateX(0)'
+      })),
+      transition('void => *', [
+        style({transform: 'translateX(-100%)'}),
+        animate('0.3s 3s')
+      ])
     ])
   ]
 })
